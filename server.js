b45a848a51c3bd73180fdabf7e3064ea25d4756c
leafartech <119262053+leafartech@users.noreply.github.com>
2023-04-18 16:54:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const sessionOpt = session({
 //CORS
 const cors = require('cors')
 const corsOptions = {
-    origin:'http://localhost:3000', 
+    origin:'https://managefly-frontend.vercel.app/', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
